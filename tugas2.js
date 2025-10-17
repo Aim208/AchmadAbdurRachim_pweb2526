939bcc0aa8073ce70acbfa4713423e314c26ef95
Aim <achmadabdurrachim8@gmail.com>
2025-10-17 09:42:28 +0700

    Add files via upload
    javaScript

---
+++ tugas2.js
@@ -0,0 +1,12 @@
+var a = 6;
+var b = 11;
+document.writeln("perkalian :"+a+"*"+b+"="+(a*B));
+document.writeln("<br>")
+document.writeln("pembagian :"+a+"/"+b+"="+(a/b));
+document.writeln("<br>")
+documen.writeln("penjumlahan :"+a+"+"+b+"="+(a+b));
+document.writeln("<br>")
+documen.writeln("pengurangan :"+a+"-"+b+"="+(a-b));
+document.writeln("<br>")
+documen.writeln("modulus :"+a+"%"+b+"="+(a%b));
+document.writeln("<br>")
---
+++ tugas5.js
@@ -0,0 +1,12 @@
+var a = 5;
+var b = 10;
+document.writeln("perkalian :"+a+"*"+b+"="+(a*B));
+document.writeln("<br>")
+document.writeln("pembagian :"+a+"/"+b+"="+(a/b));
+document.writeln("<br>")
+documen.writeln("penjumlahan :"+a+"+"+b+"="+(a+b));
+document.writeln("<br>")
+documen.writeln("pengurangan :"+a+"-"+b+"="+(a-b));
+document.writeln("<br>")
+documen.writeln("modulus :"+a+"%"+b+"="+(a%b));
+document.writeln("<br>")
